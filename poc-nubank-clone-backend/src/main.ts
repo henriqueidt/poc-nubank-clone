4b2dd50f82315e3eb17ca08a7e7d9f9eb82de3d3
Henrique Eidt <henriqueidt@gmail.com>
2024-01-23 00:28:22 -0300

    Create controllers and services

---
+++ poc-nubank-clone-backend/src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+// This is the function that initializes the server
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
+bootstrap();
